fe5ba982470fccf4b44931dff154c6824086d0e8
pabllwn <92005758+pabllwn@users.noreply.github.com>
2024-08-27 03:39:12 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -4,12 +4,12 @@ from threading import Thread
 app = Flask('')
 
 @app.route('/')
-def home():
-    return "I am alive!"
+def main():
+    return "Ready!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host="0.0.0.0", port=8080)
 
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+    server = Thread(target=run)
+    server.start() 
